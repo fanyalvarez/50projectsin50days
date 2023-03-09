192ca14eef6dba83a4b37774f40cb66f6c0255cc
fany <alvarezpestefania@outlook.com>
2023-03-08 23:51:22 -0600

    add day15

--- day15/script.js
+++ day15/script.js
@@ -5,12 +5,12 @@ counters.forEach(counter => {
 
     const updateCounter = () => {
         const target = +counter.getAttribute('data-target')
-        const c = +counter.innerText
+        const count = +counter.innerText
 
         const increment = target / 200
 
-        if(c < target) {
-            counter.innerText = `${Math.ceil(c + increment)}`
+        if(count < target) {
+            counter.innerText = `${Math.ceil(count + increment)}`
             setTimeout(updateCounter, 1)
         } else {
             counter.innerText = target
